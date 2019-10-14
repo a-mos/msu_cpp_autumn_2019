07c28d1a2603dbe58cf38176537751bb7d32ab7b
Andrey <andryha-1999@mail.ru>
2019-10-14 20:51:40 +0300

    01 fixes

--- 01/calc.cpp
+++ 01/calc.cpp
@@ -1,20 +1,20 @@
 // Grammar:
-// S = T {("+" | "-") T}
-// T = F {("*" | "/") F}
-// F = {"-"} D {D}
-// D = ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")
+// Start = Sum {("+" | "-") Sum}
+// Sum = Factor {("*" | "/") Factor}
+// Factor = {"-"} Digit {Digit}
+// Digit = ("0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9")
+#include <stdexcept>
 #include <iostream>
 #include <string>
 using namespace std;
-string s;
-int pos = -1;
-int T();
-int F();
-int S() {
-    int value = T();
+
+int Sum(string &s, int &pos);
+int Factor(string &s, int &pos);
+int Start(string &s, int &pos) {
+    int value = Sum(s, pos);
     while (s[pos + 1] == '-' || s[pos + 1] == '+') {
         int op = s[++pos];
-        int next_value = T();
+        int next_value = Sum(s, pos);
         if (op == '+') {
             value += next_value;
         } else {
@@ -24,14 +24,14 @@ int S() {
     return value;
 }
 
-int T() {
-    int value = F();
+int Sum(string &s, int &pos) {
+    int value = Factor(s, pos);
     while (s[pos + 1] == '*' || s[pos + 1] == '/') {
         int op = s[++pos];
-        int next_value = F();
+        int next_value = Factor(s, pos);
         if (op == '/') {
             if (next_value == 0) {
-                throw "Division by zero";
+                throw invalid_argument("Division by zero");
             }
             value /= next_value;
         } else {
@@ -41,7 +41,7 @@ int T() {
     return value;
 }
 
-int F() {
+int Factor(string &s, int &pos) {
     bool negative = false;
     while (s[pos + 1] == '-') {
         negative = !negative;
@@ -49,7 +49,7 @@ int F() {
     }
     int number = 0;
     if (!isdigit(s[pos + 1])) {
-        throw "Invalid expression";
+        throw invalid_argument("Invalid expression");
     }
     while (isdigit(s[pos + 1])) {
         int dig = s[++pos] - '0';
@@ -62,23 +62,34 @@ int F() {
     }
 }
 
-int main(int argc, char* argv[]) {
-    for (size_t i = 0; i < strlen(argv[1]); ++i) {
-        if (isspace(argv[1][i])) {
-             continue;
-        } else if (isdigit(argv[1][i]) || argv[1][i] == '+' \
-            || argv[1][i] == '-' || argv[1][i] == '*' || argv[1][i] == '/') {
-            s += argv[1][i];
+void get_string(string &s, char* argv) {
+    for (size_t i = 0; i < strlen(argv); ++i) {
+        if (isspace(argv[i])) {
+            continue;
+        } else if (isdigit(argv[i]) || argv[i] == '+' \
+                   || argv[i] == '-' || argv[i] == '*' || argv[i] == '/') {
+            s += argv[i];
         } else {
-            cout << "Invalid expression" << endl;
-            return 1;
+            throw invalid_argument("Invalid expression");
         }
     }
-    try {
-        int ans = S();
-        cout << ans << endl;
-    } catch (const char *err) {
-        cout << err << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        try {
+            string s;
+            int pos = -1;
+            get_string(s, argv[1]);
+            int ans = Start(s, pos);
+            cout << ans << endl;
+        } catch (exception &err) {
+            cout << err.what() << endl;
+            return 1;
+        }
+    } else {
+        cout << "No expression" << endl;
         return 1;
     }
+    return 0;
 }
