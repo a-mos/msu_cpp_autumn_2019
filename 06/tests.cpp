f9c0952301a1550c23cc18382275cb070c543813
Andrey <andryha-1999@mail.ru>
2019-11-20 20:08:09 +0300

    06

--- 06/tests.cpp
+++ 06/tests.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char *argv[]) {
     if (argc > 1) {
         switch (atoi(argv[1])) {
             case 1: {
-    			cout << format("1");
+                cout << format("1");
                 break;
             }
             case 2: {
@@ -16,44 +16,44 @@ int main(int argc, const char *argv[]) {
                 break;
             }
             case 3: {
-            	cout << format("{1}+{1}", 777, "one");
-            	break;
+                cout << format("{1}+{1}", 777, "one");
+                break;
             }
             case 4: {
-            	try {
-            		cout << format("{2}+{2}", 777);
-            	} catch (std::runtime_error& e) {
-            		cout << e.what();
-            	}
-            	break;
+                try {
+                    cout << format("{2}+{2}", 777);
+                } catch (std::runtime_error& e) {
+                    cout << e.what();
+                }
+                break;
             }
             case 5: {
-            	try {
-            		cout << format("{{{}}}");
-            	} catch (std::runtime_error& e) {
-            		cout << e.what();
-            	}
-            	break;
+                try {
+                    cout << format("{{{}}}");
+                } catch (std::runtime_error& e) {
+                    cout << e.what();
+                }
+                break;
             }
             case 6: {
-	            try {
-	          		cout << format("777{");
-	            } catch (std::runtime_error& e) {
-	            		cout << e.what();
-	            }
-	            break;
-	        }
-	        case 7: {
-	        	cout << format("{0}{1}{2}{3}", 777, "one", "two", 3);
-	        	break;
-	        }
-	        case 8: {
                 try {
-	          	    cout << format("777{");
+                    cout << format("777{");
                 } catch (std::runtime_error& e) {
                         cout << e.what();
                 }
-	        }
+                break;
+            }
+            case 7: {
+                cout << format("{0}{1}{2}{3}", 777, "one", "two", 3);
+                break;
+            }
+            case 8: {
+                try {
+                    cout << format("777{");
+                } catch (std::runtime_error& e) {
+                        cout << e.what();
+                }
+            }
             default: {
                 break;
             }
